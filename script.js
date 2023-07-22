d6722823b5a55c3658f9880573d3171030b99a4d
Ayan20april <112053665+Ayan20april@users.noreply.github.com>
2023-07-22 09:38:25 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,50 +1,55 @@
-var mealForm = document.getElementById("meal-form");
-var mealList = document.getElementById("meal-list");
-var searchInput = document.getElementById("search-input");
+/* declared three variable */
+const searchInput = document.getElementById("searchInput");
+const searchButton = document.getElementById("searchButton");
+const mealDetailsDiv = document.getElementById("mealDetails");
 
-/* this array for store the meals */
-var meals = [];
-
-/* added event listener to this form, which triggers a function when the form is submitted */
-mealForm.addEventListener("submit", function (event) {
-  event.preventDefault();
-  var mealName = document.getElementById("meal-name").value;
-  var mealDescription = document.getElementById("meal-description").value;
-
-  /* here is the values of the meal name and description inputs, creates a meal object added it to meals app */
-  /* then call displayMeals */
-  if (mealName.trim() !== "") {
-    var meal = {
-      name: mealName,
-      description: mealDescription,
-    };
-
-    meals.push(meal);
-    displayMeals();
-    mealForm.reset();
+/* added click event listener to this variable, when button is clicked */
+searchButton.addEventListener("click", () => {
+  const searchTerm = searchInput.value.trim();
+  
+  /* we check if this variable is not empty string */
+  if (searchTerm !== "") {
+    searchForMeal(searchTerm);
   }
 });
 
-/* event listener is added this input for which triggers a function whenever the input value changes */
-searchInput.addEventListener("input", function () {
-  var searchTerm = searchInput.value.toLowerCase();
-  /* search function filters the meals array based on the entered search term */
-  var filteredMeals = meals.filter(function (meal) {
-    return meal.name.toLowerCase().includes(searchTerm);
-  };
+/* this function takes a mealName as a argument and */
+/* also use template literals to construct the API URL with the mealName */
+function searchForMeal(mealName) {
+  const apiUrl = `https://www.themealdb.com/api/json/v1/1/search.php?s=${mealName}`;
 
-  /* calls this function with the filtered array */
-  displayMeals(filteredMeals);
-});
+  /* this fetch api to make a GET request to the specified URL */
+  fetch(apiUrl)
+    /* respone converted to json */
+    /* then and catch methods to handle the response data or any errors that may occur during the API request */
+    .then((response) => response.json())
+    .then((data) => {
+      if (data.meals) {
+        displayMealDetails(data.meals[0]);
+      } else {
+        mealDetailsDiv.textContent = "Meal not found.";
+      }
+    })
+    .catch((error) => {
+      mealDetailsDiv.textContent = "Error fetching data.";
+      console.error(error);
+    });
+}
 
-/* this function takes an optional array argument and clears the meal list */
-/* it iterates over the array, creates list items for each meal and appends them to the meal list */
-function displayMeals(mealsArray = meals) {
-  mealList.innerHTML = "";
-
-  mealsArray.forEach(function (meal) {
-    var listItem = document.createElement("li");
-    listItem.textContent = meal.name + " - " + meal.description;
-    mealList.appendChild(listItem);
-  });
+/* this function takes a meal object as an argument */
+/* its properties to get the relevant meal details.*/
+function displayMealDetails(meal) {
+  const { strMeal, strCategory, strArea, strInstructions, strMealThumb } = meal;
+  
+  const mealDetailsHTML = `
+    <h2>${strMeal}</h2>
+    <p><strong>Category:</strong> ${strCategory}</p>
+    <p><strong>Area:</strong> ${strArea}</p>
+    <p><strong>Instructions:</strong> ${strInstructions}</p>
+    <img src="${strMealThumb}" alt="${strMeal}">
+  `;
+  
+  /* The mealDetailsHTML is then set as the innerHTML of the mealDetailsDiv */
+  /* effectively displaying the meal details on the page */
+  mealDetailsDiv.innerHTML = mealDetailsHTML;
 }
